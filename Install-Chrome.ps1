d8b765ed2524100a82c7e7dc423f70c9ea07489a
TheKingMarco <108090496+TheKingMarco@users.noreply.github.com>
2022-06-23 20:15:27 +0200

    Add files via upload

---
+++ Install-Chrome.ps1
@@ -0,0 +1,5 @@
+﻿md -Path $env:temp\chromeinstall -erroraction SilentlyContinue | Out-Null
+$Download = join-path $env:temp\chromeinstall chrome_installer.exe
+Invoke-WebRequest 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe'  -OutFile $Download
+Invoke-Expression "$Download"
+#Invoke-Expression "$Download /silent /install"
