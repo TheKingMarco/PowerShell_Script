b8fece0ae988a108fb18b49440ef5b25606c10c7
Marco Oliva <oliva.marco00@gmail.com>
2022-11-25 18:46:17 +0100

    update orario in aggiornameto app choco script

--- Aggiornamento-App-PC-Choco.ps1
+++ Aggiornamento-App-PC-Choco.ps1
@@ -12,6 +12,6 @@ if (Get-ScheduledTask -TaskName "daily-check-app-update"){
 }
 else{
     $action = New-ScheduledTaskAction -Execute  'powershell.exe' -Argument "C:\Users\oliva\IDrive-Sync\Documenti\Script-Powershell\Aggiornamento-App-PC-Choco.ps1"
-    $trigger = New-ScheduledTaskTrigger -Daily -At 11
+    $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
     Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath "MyTask" -TaskName "daily-check-app-update" -Description "Controllo giornalmente se sono ci sono aggiornamenti sulle app installate nel pc e in caso le aggiorna"
 }
