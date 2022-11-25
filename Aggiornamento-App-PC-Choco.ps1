45f8bd8eaa0012c01fc95e702417a0c62c8edb27
Marco Oliva <oliva.marco00@gmail.com>
2022-11-25 18:51:50 +0100

    update aggiornameto app choco script

--- Aggiornamento-App-PC-Choco.ps1
+++ Aggiornamento-App-PC-Choco.ps1
@@ -1,9 +1,14 @@
 #script per aggiornare applicazioni installate con choco
 #todo:
 <#
-creare uno scadule task che permetta l'esecuzuine automatica dello script ogni (tot tempo, o trigger)
-aggiorna tutte le app 
+creare uno scadule task che permetta l'esecuzuine automatica dello script ogni (tot tempo, o trigger) (OK)
+aggiorna tutte le app (OK)
 #>
+
+###-----Attenzione-----------###
+#Cambiare il parametro -Argument con il path dello script
+
+
 #$Packages = 'googlechrome', 'git', 'notepadplusplus', 'everything' , 'bitwarden' , 'bitwarden-chrome' ,'ditto' ,'revo-uninstaller','eartrumpet', 'gimp', 'idrive','vscode','azure-cli','notion','vscode-ansible','wsl','pdftk','obs-studio','putty','sharex','telegram','whatsapp','vlc','winrar','winscp','vscode-terraform','terraform'
 $Packages = 'bitwarden-chrome', 'patch-my-pc' ,'eartrumpet','idrive','azure-cli','wsl','terraform'
 
@@ -11,7 +16,7 @@ if (Get-ScheduledTask -TaskName "daily-check-app-update"){
     choco upgrade $Packages -y
 }
 else{
-    $action = New-ScheduledTaskAction -Execute  'powershell.exe' -Argument "C:\Users\oliva\IDrive-Sync\Documenti\Script-Powershell\Aggiornamento-App-PC-Choco.ps1"
+    $action = New-ScheduledTaskAction -Execute  'powershell.exe' -Argument "<PATHS_CRIPT>"
     $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
     Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath "MyTask" -TaskName "daily-check-app-update" -Description "Controllo giornalmente se sono ci sono aggiornamenti sulle app installate nel pc e in caso le aggiorna"
 }
